52fe502a9fc9523a62f2cfe6c095e969de0be037
Alyce <Alyceosbourne@outlook.com>
2022-12-16 23:42:56 +0000

    Site

--- flask_website.py
+++ flask_website.py
@@ -1,17 +1,43 @@
 import pathlib
+from typing import NamedTuple
+from datetime import date
 import flask
 import random
 from github_tools import get_repos, OWNER_NAME
+import json
+import sqlalchemy
 
 app = flask.Flask(__name__)
-pathlib.Path(app.static_folder).mkdir(parents = True, exist_ok = True)
-pathlib.Path(app.template_folder).mkdir(parents = True, exist_ok = True)
+
 static_path = pathlib.Path('static')
+
 artwork = static_path / 'artwork'
+blog_entry_path = static_path / 'blog_entries'
+
+artwork.mkdir(parents = True, exist_ok = True)
+blog_entry_path.mkdir(parents = True, exist_ok = True)
+
+
+class BlogPost(NamedTuple):
+    title: str
+    date: date
+    content: str
+    tags: list
+
+    def to_json(self):
+        return json.dumps(self._asdict())
+
+    @classmethod
+    def from_json(cls, json_string):
+        return cls(**json.loads(json_string))
 
 
 def get_artwork():
     return random.sample([str(image) for image in artwork.iterdir()], 3)
+
+
+def get_blog_posts():
+    return [BlogPost.from_json(post.read_text()) for post in blog_entry_path.iterdir()]
 
 
 @app.route('/')
@@ -26,8 +52,11 @@ def index():
 
 @app.route('/blog')
 def blog():
-    return flask.render_template('blog.html')
+    return flask.render_template(
+            'blog.html',
+            posts = get_blog_posts(),
+    )
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host = '0.0.0.0', debug = True)
